4add7ea29803eff2a240c63a3d29c1ae2f9127f2
EduLpz21 <EduLpz21@users.noreply.github.com>
2023-01-23 16:59:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("htmlcss");
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("php");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("csharp");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[7].classList.add("organizacion");
+        habilidades[8].classList.add("flexibilidad");
+        habilidades[9].classList.add("auto");
     }
 }
 
